4f2f13b5647b79697dc02737540e4de92f4ba03d
mudkipdev <mudkip@mudkip.dev>
2023-09-20 16:30:07 -0600

    reformat code

--- scnewsbot/extensions/announcements.py
+++ scnewsbot/extensions/announcements.py
@@ -7,7 +7,7 @@ import discord
 from utils import can_publish_announcements
 
 ANNOUNCEMENT_BUILDER_TIMEOUT = 1200
-ANNOUNCEMENT_EMOJI = "👍" # <:upvote:354233015842635776>
+ANNOUNCEMENT_EMOJI = "👍"  # <:upvote:354233015842635776>
 DEFAULT_IMAGE_URL = "https://media.discordapp.net/attachments/1062905729532571719/1123340546979676311/NewsDefault.jpg?width=810&height=180"
 INSTRUCTIONS = """\
 1. Title should not use any formatting.
@@ -189,7 +189,9 @@ class Announcement:
         setattr(self, option.id, converted_value)
         return True
 
-    async def get_embed(self, *, bot: commands.Bot, show_author: bool = False) -> discord.Embed:
+    async def get_embed(
+        self, *, bot: commands.Bot, show_author: bool = False
+    ) -> discord.Embed:
         embed = discord.Embed(
             color=bot.config.embed_color,
             title=self.title,
